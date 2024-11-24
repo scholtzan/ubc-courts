03ae97f7292e933f9acf4759af55ccc87d76b31d
Anna Scholtz <anna@scholtzan.net>
2024-11-23 20:29:24 -0800

    Add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
